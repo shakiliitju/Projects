fcc8ee9943812b835522d524b98342b86fd11865
Md. Shakil Hossain <shakiljusc15@gmail.com>
2022-03-31 07:06:38 +0600

    update

---
+++ Library Management System of IIT/src/Project/DbPK.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Project;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+/**
+ *
+ * @author USER
+ */
+@Embeddable
+public class DbPK implements Serializable {
+
+    @Basic(optional = false)
+    @Column(name = "Host")
+    private String host;
+    @Basic(optional = false)
+    @Column(name = "Db")
+    private String db;
+    @Basic(optional = false)
+    @Column(name = "User")
+    private String user;
+
+    public DbPK() {
+    }
+
+    public DbPK(String host, String db, String user) {
+        this.host = host;
+        this.db = db;
+        this.user = user;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public String getDb() {
+        return db;
+    }
+
+    public void setDb(String db) {
+        this.db = db;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (host != null ? host.hashCode() : 0);
+        hash += (db != null ? db.hashCode() : 0);
+        hash += (user != null ? user.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof DbPK)) {
+            return false;
+        }
+        DbPK other = (DbPK) object;
+        if ((this.host == null && other.host != null) || (this.host != null && !this.host.equals(other.host))) {
+            return false;
+        }
+        if ((this.db == null && other.db != null) || (this.db != null && !this.db.equals(other.db))) {
+            return false;
+        }
+        if ((this.user == null && other.user != null) || (this.user != null && !this.user.equals(other.user))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Project.DbPK[ host=" + host + ", db=" + db + ", user=" + user + " ]";
+    }
+    
+}
